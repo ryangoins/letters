dc8fd16f0cbda136f88e170db2e020e37adf5b58
ryangoins <ryan.c.goins@gmail.com>
2014-04-22 21:36:06 -0400

    generated Letter model

---
+++ app/views/letters/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @letter, :id, :recipient_first_name, :recipeint_last_name, :recipient_street, :recipient_city, :recipient_state, :recipient_zip, :content, :created_at, :updated_at
---
+++ db/migrate/20140423013446_create_letters.rb
@@ -0,0 +1,15 @@
+class CreateLetters < ActiveRecord::Migration
+  def change
+    create_table :letters do |t|
+      t.string :recipient_first_name
+      t.string :recipeint_last_name
+      t.string :recipient_street
+      t.string :recipient_city
+      t.string :recipient_state
+      t.integer :recipient_zip
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
